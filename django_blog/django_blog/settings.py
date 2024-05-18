36c1c682681d6ae60555a846bb3dc29f7d1c635e
TomaAghakhanyan <t.aghakhanyan@gmail.com>
2024-05-18 21:28:02 +0400

    django

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-f#gzvgpjtpr7y26xa-w*6%c$1(7bac#(011w855yru&oniko_z'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
 
