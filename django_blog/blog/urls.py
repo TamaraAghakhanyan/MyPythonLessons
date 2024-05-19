e881732097c8b4b96cf0456ff71fb65dfa728b22
TomaAghakhanyan <t.aghakhanyan@gmail.com>
2024-05-20 00:53:59 +0400

    adding django blog/urls

--- django_blog/blog/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import include, path
-from blog.views import home
-
-
-urlpatterns = [
-    path('', home)
-]
--- django_blog/blog/views.py
+++
@@ -1,8 +0,0 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-
-from django.urls import include, path
-
-# Create your views here.
-def home(rquest):
-    return HttpResponse("Welcome to my blog")
